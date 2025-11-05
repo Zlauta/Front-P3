e0ce8720f99b46decdf19177fff8106e13abab5f
Zlauta <Zlauuta23@gmail.com>
2025-11-05 16:10:57 -0300

    feat(routes): add router components and register route subtrees

--- src/routes/PublicRoute.jsx
+++ src/routes/PublicRoute.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Carta from "../pages/user/Carta";
+import Galeria from "../pages/user/Galeria";
+import SobreNosotros from "../pages/user/SobreNosotros";
+import Contacto from "../pages/user/Contacto";
+import Home from "../pages/user/Home";
+
+import Login from "../pages/auth/Login";
+import Register from "../pages/auth/Register";
 
 const PublicRoute = () => {
   return (
-    <div>PublicRoute</div>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/carta" element={<Carta />} />
+      <Route path="/galeria" element={<Galeria />} />
+      <Route path="/sobre-nosotros" element={<SobreNosotros />} />
+      <Route path="/contacto" element={<Contacto />} />
 
-export default PublicRoute
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
+};
+
+export default PublicRoute;
