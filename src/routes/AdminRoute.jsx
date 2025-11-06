cdb22cbca0b705be222c61fb1e646a10d75a7ed8
Zlauta <Zlauuta23@gmail.com>
2025-11-05 23:57:18 -0300

    feat(routes): implementar AdminRoute con estructura de rutas administrativas y layout

--- src/routes/AdminRoute.jsx
+++ src/routes/AdminRoute.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-
+import { Routes, Route, Navigate, Outlet } from "react-router-dom";
+import SidebarAdmin from "../layout/SidebarAdmin";
 
 import Usuarios from "../pages/admin/Usuarios";
 import Menu from "../pages/admin/Menu";
@@ -10,17 +10,31 @@ import Reservas from "../pages/admin/Reservas";
 import Contacto from "../pages/admin/Contacto";
 import Promociones from "../pages/admin/Promociones";
 
+const AdminLayout = () => {
+  return (
+    <div className="admin-layout">
+      <SidebarAdmin />
+      <main className="admin-content">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
 const AdminRoute = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/admin/usuarios" replace />} />
-      <Route path="/usuarios" element={<Usuarios />} />
-      <Route path="/menu" element={<Menu />} />
-      <Route path="/pedidos" element={<Pedidos />} />
-      <Route path="/resenias" element={<Resenias />} />
-      <Route path="/reservas" element={<Reservas />} />
-      <Route path="/contacto" element={<Contacto />} />
-      <Route path="/promociones" element={<Promociones />} />
+      {/* Use AdminLayout as wrapper for all admin child routes (mounted at /admin/*) */}
+      <Route path="/" element={<AdminLayout />}>
+        <Route index element={<Navigate to="/admin/usuarios" replace />} />
+        <Route path="usuarios" element={<Usuarios />} />
+        <Route path="menu" element={<Menu />} />
+        <Route path="pedidos" element={<Pedidos />} />
+        <Route path="resenias" element={<Resenias />} />
+        <Route path="reservas" element={<Reservas />} />
+        <Route path="contacto" element={<Contacto />} />
+        <Route path="promociones" element={<Promociones />} />
+      </Route>
 
       <Route path="*" element={<Navigate to="/admin/usuarios" replace />} />
     </Routes>
