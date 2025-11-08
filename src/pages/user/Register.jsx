f267440b88aa24224feeaee84d5d8a3df518c04c
Amelia A <maantonib@yahoo.com.ar>
2025-11-08 17:31:58 -0300

    Se implementa formulario de registro de usuarios con validaciones iniciales. Se incorpora dependencia para mostrar alertas y se integra dependencia para envío de correos automáticos al registrar un nuevo usuario

--- src/pages/user/Register.jsx
+++ src/pages/user/Register.jsx
@@ -1,5 +1,25 @@
-const Register = () => {
-  return <div>Register nuevo</div>;
+import React from "react";
+import { Container } from "react-bootstrap";
+import FormRegister from "../auth/FormRegister";
+import "./login.css";
+import { Link } from "react-router-dom";
+
+const RegisterPage = () => {
+  return (
+    <>
+      <h1 className="titulos-form">Registrarse </h1>
+      <Container className="custom-form border rounded p-4 w-50 mb-5">
+        <FormRegister></FormRegister>
+        <div className="mt-4 text-center">
+          <div className="mt-2">
+            <Link to="/" className="link-color text-decoration-none">
+              ← Volver al inicio
+            </Link>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
 };
 
-export default Register;
+export default RegisterPage;
