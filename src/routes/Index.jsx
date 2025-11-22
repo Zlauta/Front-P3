6c623f37cbcd08b401ec1647930577fc4d081134
Zlauta <Zlauuta23@gmail.com>
2025-11-22 12:10:36 -0300

    feat(api): create interceptor component and integrate visual alerts and integrate AxiosInterceptor into main router

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -1,24 +1,22 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import AdminRoute from "./AdminRoute";
 import PublicRoute from "./PublicRoute";
 import NotFound from "../pages/user/NotFound.jsx";
-
-
+import AxiosInterceptor from "../components/manejoErrores/AxiosInterceptor.jsx";
 
 const Index = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        
-        <Route path="/404" element={<NotFound />} />
+      <AxiosInterceptor>
+        <Routes>
+          <Route path="/404" element={<NotFound />} />
 
-        <Route path="/admin/*" element={<AdminRoute />} />
+          <Route path="/admin/*" element={<AdminRoute />} />
 
-        <Route path="/*" element={<PublicRoute />} />
-
-
-      </Routes>
+          <Route path="/*" element={<PublicRoute />} />
+        </Routes>
+      </AxiosInterceptor>
     </BrowserRouter>
   );
 };
