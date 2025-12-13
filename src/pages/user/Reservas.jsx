4f0772a02dc1059a98b0f6e018175532a2c938d9
Amelia A <maantonib@yahoo.com.ar>
2025-12-13 19:04:20 -0300

    feat(reservations): add obtenerMisReservas endpoint with email and date filtering

--- src/pages/user/Reservas.jsx
+++ src/pages/user/Reservas.jsx
@@ -1,8 +1,10 @@
 import FormReserva from "../../components/reserva/FormularioReserva";
+import MisReservas from "../../pages/user/MisReservas";
 
 export default function Reservas() {
   return (
     <div>
+      <MisReservas />
       <FormReserva />
     </div>
   );
